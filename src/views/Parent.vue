0681d3782603f13cc1fd34d545cc31eb493a6be4
yangchaojun <yangchaojunxs@126.com>
2019-11-15 09:06:36 +0800

    vue 动态删除对象

--- src/views/Parent.vue
+++ src/views/Parent.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
     I am parent.
+    <p v-for="(data,key) in dataSource" :key="key" @click="handleDetele(key)">
+      <span v-for="(item,index) in data" :key="index">
+        {{item.a}}
+      </span>
+    </p>
     <router-view />
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      dataSource: {}
+    }
+  },
+  mounted() {
+    this.dataSource = {
+        '122': [{
+          a: 'xxx'
+        }, {
+          a: 'xxx'
+        }],
+        '123': [{
+          a:'zzz'
+        }]
+      }
+  },
+  methods: {
+    handleDetele(key) {
+      this.$delete(this.dataSource, key)
+    }
+  }
 }
 </script>
 
